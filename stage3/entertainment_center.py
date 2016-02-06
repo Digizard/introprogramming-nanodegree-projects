7717ef7e243223eef2890346b0e490e57bd07e96
Sitting Fox <SittingFox@users.noreply.github.com>
2016-02-05 21:56:32 -0500

    feat: Implement Video class in Stage 3, replacing Movie class

--- stage3/entertainment_center.py
+++ stage3/entertainment_center.py
@@ -1,18 +1,14 @@
-import media
 import fresh_tomatoes
+import media
 
-red_trailer = media.Movie("Red Trailer", "Show of RWBY concept with the character Ruby Rose.",
-                  "http://lorempixel.com/400/200/", "https://www.youtube.com/watch?v=pYW2GmHB5xs")
+red_trailer = media.Video("Red Trailer", "https://www.youtube.com/watch?v=pYW2GmHB5xs")
 
-white_trailer = media.Movie("White Trailer", "Second RWBY trailer, about character Weiss Schnee.",
-                  "http://lorempixel.com/400/200/", "https://www.youtube.com/watch?v=Vt9vl8iAN5Q")
+white_trailer = media.Video("White Trailer", "https://www.youtube.com/watch?v=Vt9vl8iAN5Q")
 
-black_trailer = media.Movie("Black Trailer", "Third RWBY trailer, about character Blake Belladonna.",
-                  "http://lorempixel.com/400/200/", "https://www.youtube.com/watch?v=ImKCt7BD4U4")
+black_trailer = media.Video("Black Trailer", "https://www.youtube.com/watch?v=ImKCt7BD4U4")
 
-yellow_trailer = media.Movie("Yellow Trailer", "Final RWBY trailer, about character Yang Xiao Long.",
-                  "http://lorempixel.com/400/200/", "https://www.youtube.com/watch?v=QCw_aAS7vWI")
+yellow_trailer = media.Video("Yellow Trailer", "https://www.youtube.com/watch?v=QCw_aAS7vWI")
 
 
-movies = [red_trailer, white_trailer, black_trailer, yellow_trailer]
-fresh_tomatoes.open_movies_page(movies)
+videos = [red_trailer, white_trailer, black_trailer, yellow_trailer]
+fresh_tomatoes.open_videos_page(videos)
--- stage3/media.py
+++ stage3/media.py
@@ -1,15 +1,21 @@
+import re
 import webbrowser
 
-class Movie():
-    """This class provides a way to store movie related information."""
+class Video():
+    """Video-related information."""
 
-    def __init__(self, movie_title, movie_storyline, poster_image,
-                 trailer_youtube):
-        # initialize instance of class Movie
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+    def __init__(self, video_title, youtube_url):
+        # initialize instance of class Video
+        self.title = video_title
+        self.youtube_url = youtube_url
+        self.setup_youtube_id()
+        self.thumbnail_url = "http://img.youtube.com/vi/" + self.youtube_id + "/mqdefault.jpg"
 
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
+    def show(self):
+        webbrowser.open(self.youtube_url)
+
+    def setup_youtube_id(self):
+         # Extract the youtube ID from the url, thanks to original fresh_tomatoes.py code!
+        youtube_id_match = re.search(r'(?<=v=)[^&#]+', self.youtube_url) or re.search(r'(?<=be/)[^&#]+', self.youtube_url)
+        self.youtube_id = (youtube_id_match.group(0) if youtube_id_match
+                              else None)
